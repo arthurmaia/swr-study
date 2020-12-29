ca5240a0b71b1536c7c71151eb9e320d3c282aa3
Arthur Maia <arthur.maia.dev@gmail.com>
2020-12-29 00:40:54 -0300

    Add Routes component to main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Routes from "./routes";
+
 const App: React.FC = () => {
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
+			<Routes />
 		</div>
 	);
 };
